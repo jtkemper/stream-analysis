3f82f16491a1afbe080e828270b9fd04b983a25c
Danyka Byrnes <danyka.byrnes@gmail.com>
2026-03-18 15:25:37 -0400

    adding discharge analysis, seeing where the solute samples fall within the discharge range

--- config.py
+++ config.py
@@ -1,6 +1,26 @@
 # Input and Output filepath
 INPUT_filepath = "../INPUT/"
+geospat_data_filepath = INPUT_filepath+"00_geospatial_data/"
+ancillary_data_filepath = INPUT_filepath+"00_ancillary_data/"
+basin_atlas_filepath = INPUT_filepath+"04_static_basinatlas_catchment_characteristics/"
 metadata_filepath = INPUT_filepath+"01_metadata/"
 shapefile_filepath = INPUT_filepath+'03_shapefiles'
-geospat_data_filepath = INPUT_filepath+"00_geospatial_data/"
-OUTPUT_filepath = "../OUTPUT/1_analysis_files_figures/"
+water_quality_filepath = INPUT_filepath + "02_waterquality_timeseries/"
+discharge_filepath = INPUT_filepath + "09_streamflow_discharge/"
+OUTPUT_filepath = "../OUTPUT/"
+
+params_pretty_dict = {'WTemp_C':'Temp',
+                      'DO_mgL':'DO',
+                      'SpC_uScm':'SpC',
+                      'Turb_FNU':'Turb. (FNU)',
+                      'Turb_NTU':'Turb. (NTU)',
+                      'NO3_mgNL':'NO3',
+                      'fDOM_QSU':'fDOM (QSU)',
+                      'fDOM_RFU':'fDOM (RFU)',
+                      'DOC_mgL':'DOC',
+                      'PO4_mgL':'PO4',
+                      'Chla_ugL':'Chla',
+                      'Chla_RFU':'Ch;a (RFU)',
+                      'PC_ugL':'PC',
+                      'PC_RFU':'PC (RFU)',
+                      'pH':'pH'}
