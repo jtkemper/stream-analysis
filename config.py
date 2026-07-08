487ecc0cdafd6bf26864c4e290f11de9f6a25a4a
Danyka Byrnes <danyka.byrnes@gmail.com>
2026-07-07 22:37:45 -0400

    updated filepaths and script to use new STREAM data

--- config.py
+++ config.py
@@ -2,11 +2,11 @@
 INPUT_filepath = "../INPUT/"
 geospat_data_filepath = INPUT_filepath+"00_geospatial_data/"
 ancillary_data_filepath = INPUT_filepath+"00_ancillary_data/"
-basin_atlas_filepath = INPUT_filepath+"04_static_basinatlas_catchment_characteristics/"
+basin_atlas_filepath = INPUT_filepath+"04_static_catchment_characteristics/"
 metadata_filepath = INPUT_filepath+"01_metadata/"
 shapefile_filepath = INPUT_filepath+'03_shapefiles/'
 water_quality_filepath = INPUT_filepath + "02_waterquality_timeseries/"
-discharge_filepath = INPUT_filepath + "09_streamflow_discharge/"
+discharge_filepath = INPUT_filepath + "08_streamflow_discharge/"
 OUTPUT_filepath = "../OUTPUT/"
 
 params_pretty_dict = {'WTemp_C':'Temp',
@@ -18,7 +18,7 @@ params_pretty_dict = {'WTemp_C':'Temp',
                       'fDOM_QSU':'fDOM (QSU)',
                       'fDOM_RFU':'fDOM (RFU)',
                       'DOC_mgL':'DOC',
-                      'PO4_mgL':'PO4',
+                      'PO4_mgPL':'PO4',
                       'Chla_ugL':'Chla',
                       'Chla_RFU':'Ch;a (RFU)',
                       'PC_ugL':'PC',
