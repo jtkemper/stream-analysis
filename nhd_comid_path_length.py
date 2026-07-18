9281a44dac850952cf366ad8a77c0dffc59f704b
Danyka Byrnes <danyka.byrnes@gmail.com>
2026-07-18 17:00:54 -0400

    pushing changes to git before making final draft figure changes

--- nhd_comid_path_length.py
+++ nhd_comid_path_length.py
@@ -5,37 +5,15 @@ Created on Tue Mar 17 13:13:31 2026
 @author: danyk
 """
 
-from pynhd import NLDI, WaterData, NHDPlusHR
+from pynhd import NLDI #, WaterData, NHDPlusHR
 import pynhd as nhd
 import pandas as pd
 from config import *
 
 nldi = NLDI()
-md = pd.read_csv(metadata_filepath+'metadata.csv', dtype={'sourceID':str})
-
-comid_id= []
-
-# Go through metadata and fetch COMIDs
-
-for i, row in md.iterrows():
-    try:
-        comid_closest = nldi.comid_byloc((row['longitude_wgs84'], 
-                                          row['latitude_wgs84']),
-                                         loc_crs=4326)
-        comid_closest['STREAM_ID'] = row['STREAM_ID']
-        comid_id.append(comid_closest[['STREAM_ID', 'reachcode', 'comid']])
-        print(i)
-        
-    except Exception as e:
-        print(f"Failed for row {i}: {e}")
-        comid_id.append(None)
-    
-comid_df = pd.concat([x for x in comid_id if x is not None], ignore_index=True)
-
-# downloading Value-Added Attributes (VAAs)
+comid_df = pd.read_csv(metadata_filepath+'metadata.csv', dtype={'sourceID':str})
 vaa = nhd.nhdplus_vaa()
 comid_df['comid'] = comid_df['comid'].astype(int)
-comid_df = comid_df.merge(vaa[['comid', 'pathlength']], on='comid', how='left')
 
 comid_df.to_csv(INPUT_filepath+'00_ancillary_data/geomorph_data/nhdplus_pathlength.csv',
                index=False)
